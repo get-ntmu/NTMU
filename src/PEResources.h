0ae0c29d993c5802004a538c43a2788d66b0c680
aubymori <aubyomori@gmail.com>
2025-05-19 09:24:40 -0500

    Revert "unfinished icon code"
    This reverts commit ea3263cfd795ae932a4bf1d12eb0cb3866a8506f.

--- src/PEResources.h
+++ src/PEResources.h
@@ -5,42 +5,6 @@
 #include <vector>
 #include <memory>
 
-// the common header of RT_CURSOR, RT_ICON, icon/cursor files
-typedef struct ICONDIR
-{
-	WORD           idReserved;  // Reserved (must be 0)
-	WORD           idType;      // RES_ICON or RES_CURSOR
-	WORD           idCount;     // How many images?
-} ICONDIR, *LPICONDIR;
-
-// the entry of group icon resource (RT_GROUP_ICON)
-#include <pshpack2.h>
-typedef struct GRPICONDIRENTRY
-{
-	BYTE   bWidth;              // Width, in pixels, of the image
-	BYTE   bHeight;             // Height, in pixels, of the image
-	BYTE   bColorCount;         // Number of colors in image (0 if >=8bpp)
-	BYTE   bReserved;           // Reserved
-	WORD   wPlanes;             // Color Planes
-	WORD   wBitCount;           // Bits per pixel
-	DWORD  dwBytesInRes;        // how many bytes in this resource?
-	WORD   nID;                 // the ID
-} GRPICONDIRENTRY, *LPGRPICONDIRENTRY;
-#include <poppack.h>
-
-// the entry of group cursor resource (RT_GROUP_CURSOR)
-#include <pshpack2.h>
-typedef struct GRPCURSORDIRENTRY
-{
-	WORD    wWidth;             // Width, in pixels, of the image
-	WORD    wHeight;            // Height, in pixels, of the image
-	WORD    wPlanes;            // Must be 1
-	WORD    wBitCount;          // Bits per pixel
-	DWORD   dwBytesInRes;       // how many bytes in this resource?
-	WORD    nID;                // the ID
-} GRPCURSORDIRENTRY, *LPGRPCURSORDIRENTRY;
-#include <poppack.h>
-
 struct PEResourceType
 {
 	bool fIsOrdinal;
@@ -90,44 +54,6 @@ struct PEResource
 	}
 };
 
-inline void DeleteResource(std::vector<PEResource> &resources, const PEResourceType &type, const PEResourceType &name)
-{
-	for (auto &res : resources)
-	{
-		if (res.type == type && res.name == name)
-		{
-			auto it = std::find_if(resources.begin(), resources.end(), [&](PEResource &item)
-			{
-				return &item == &res;
-			});
-			resources.erase(it);
-			break;
-		}
-	}
-}
-
-inline PEResource *_FindResource(std::vector<PEResource> &resources, const PEResourceType &type, const PEResourceType &name)
-{
-	for (auto &res : resources)
-	{
-		if (res.type == type && res.name == name)
-		{
-			return &res;
-		}
-	}
-	return nullptr;
-}
-
-inline WORD LowestAvailableID(std::vector<PEResource> &resources, const PEResourceType &type)
-{
-	for (WORD i = 1;; i++)
-	{
-		PEResourceType name(MAKEINTRESOURCEW(i));
-		if (!_FindResource(resources, type, name))
-			return i;
-	}
-}
-
 inline bool LoadResources(LPCWSTR pszFile, bool fTryRes, std::vector<PEResource> &result)
 {
 	HRESULT hr;
@@ -151,79 +77,10 @@ inline bool LoadResources(LPCWSTR pszFile, bool fTryRes, std::vector<PEResource>
 	return fSucceeded;
 }
 
-void MergeResources(std::vector<PEResource> &from, std::vector<PEResource> &to)
+void MergeResources(const std::vector<PEResource> &from, std::vector<PEResource> &to)
 {
-	// HACKHACKHACK: We need to mess with icon resources as to not fuck shit over.
-	// Move them all to the end so we process them last.
-	// endless loop i fucking HATE C++
-	for (auto &res : from)
-	{
-		if (res.type.fIsOrdinal
-		&& (res.type.wOrdinal == (WORD)RT_ICON || res.type.wOrdinal == (WORD)RT_CURSOR))
-		{
-			auto it = std::find_if(from.begin(), from.end(), [&](PEResource &item)
-			{
-				return &item == &res;
-			});
-			std::rotate(it, it + 1, from.end());
-		}
-	}
-
 	for (const auto &res : from)
 	{
-		// Mess with icons
-		if (res.type.fIsOrdinal
-		&& (res.type.wOrdinal == (WORD)RT_GROUP_ICON || res.type.wOrdinal == (WORD)RT_GROUP_CURSOR))
-		{
-			bool fCursor = res.type.wOrdinal == (WORD)RT_GROUP_CURSOR;
-
-			// If the dest file has this icon group, delete all the corresponding icons
-			PEResource *pOrigResource = _FindResource(to, res.type, res.name);
-			if (pOrigResource)
-			{
-				ICONDIR *pIconDir = (ICONDIR *)&pOrigResource->data[0];
-				void *pData = (void *)(pIconDir + 1);
-				for (int i = 0; i < pIconDir->idCount; i++)
-				{
-					WORD nID;
-					if (fCursor)
-						nID = ((GRPCURSORDIRENTRY *)pData)[i].nID;
-					else
-						nID = ((GRPICONDIRENTRY *)pData)[i].nID;
-
-					PEResourceType type(fCursor ? RT_CURSOR : RT_ICON);
-					PEResourceType name(MAKEINTRESOURCEW(nID));
-					DeleteResource(to, type, name);
-				}
-			}
-
-			// Modify our own icons to make sure IDs don't conflict
-			ICONDIR *pIconDir = (ICONDIR *)&res.data[0];
-			void *pData = (void *)(pIconDir + 1);
-			for (int i = 0; i < pIconDir->idCount; i++)
-			{
-				WORD *pID;
-				if (fCursor)
-					pID = &((GRPCURSORDIRENTRY *)pData)[i].nID;
-				else
-					pID = &((GRPICONDIRENTRY *)pData)[i].nID;
-
-				PEResourceType type(fCursor ? RT_CURSOR : RT_ICON);
-				PEResourceType name(MAKEINTRESOURCEW(*pID));
-				if (_FindResource(to, type, name))
-				{
-					WORD newID = LowestAvailableID(to, type);
-					PEResource *pIconRes = _FindResource(from, type, name);
-					if (pIconRes)
-					{
-						PEResourceType newName(MAKEINTRESOURCEW(newID));
-						*pID = newID;
-						pIconRes->name = newName;
-					}
-				}
-			}
-		}
-
 		// Find and replace the resource if one with the same name, type, and lang exists
 		bool fReplaced = false;
 		for (auto &toRes : to)
