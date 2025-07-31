29dd77544d100f70128f112ea644de059743f03b
aubymori <aubyomori@gmail.com>
2025-07-30 22:49:50 -0500

    Bump version

--- src/Resource.h
+++ src/Resource.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#define VER_MAJOR                0
-#define VER_MINOR                9
+#define VER_MAJOR                1
+#define VER_MINOR                0
 #define VER_REVISION             0
-#define VER_STRING         "0.9.0"
+#define VER_STRING         "1.0.0"
 
 #define IDI_NTMU               100
 
