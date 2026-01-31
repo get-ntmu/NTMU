a27417927696b1d181a53258081c2f7e9945f89f
aubymori <aubyomori@gmail.com>
2026-01-30 22:47:25 -0600

    Make VER_STRING define dynamic

--- src/Resource.h
+++ src/Resource.h
@@ -1,8 +1,13 @@
 #pragma once
 
+#define STRINGIZE_(s) #s
+#define STRINGIZE(s)  STRINGIZE_(s)
+
 #define VER_MAJOR                1
 #define VER_MINOR                0
 #define VER_REVISION             0
-#define VER_STRING         "1.0.0"
+
+#define VER_STRING \
+    STRINGIZE(VER_MAJOR) "." STRINGIZE(VER_MINOR) "." STRINGIZE(VER_REVISION)
 
 #define IDI_NTMU               100
