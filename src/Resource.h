5454ceb7b732e49e5a5b82e12b7ce7b8bea551ac
aubymori <aubyomori@gmail.com>
2026-01-30 22:37:45 -0600

    Remove leftover string resources

--- src/Resource.h
+++ src/Resource.h
@@ -6,15 +6,3 @@
 #define VER_STRING         "1.0.0"
 
 #define IDI_NTMU               100
-
-#define IDS_MAINWND            400
-#define IDS_PACKNAME           401
-#define IDS_PACKAUTHOR         402
-#define IDS_PACKVERSION        403
-#define IDS_APPLY              404
-#define IDS_NOPREVIEW          405
-#define IDS_NOREADME           406
-#define IDS_NOOPTIONS          407
-#define IDS_NOPACKLOADED       408
-#define IDS_NOTSPECIFIED       409
-#define IDS_CLOSE              410
